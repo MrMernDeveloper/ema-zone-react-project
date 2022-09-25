839375829a6c11ce4c582aa6d40be01a6b8f1c4b
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-26 01:51:37 +0600

    added some code

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,18 +4,22 @@ import './Cart.css'
 const Cart = ({ cart }) => {
     console.log(cart);
     let total = 0;
+    let shipping = 0;
     
     for (const product of cart) {
         total = total + product.price;
+        shipping = shipping + product.shipping
     }
+
+    const tax = (total * 0.1).toFixed(2);
     
     return (
         <div className='cart'>
             <h3 className='order-summary'>Order Summary</h3>
             <p>Selected Item: {cart.length}</p>
             <p>Total Price: ${total}</p>
-            <p>Total Shipping:</p>
-            <p>Tax:</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
             <h5>Grand Total:</h5>
         </div>
     );
